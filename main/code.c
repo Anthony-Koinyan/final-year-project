3fbcfe64bca5862ff4ad2a7f7ffe86c466bfadd4
Anthony-Koinyan <aaa.koinyan@gmail.com>
2025-06-25 00:00:43 +0100

    embed jerryscript

--- main/code.c
+++ main/code.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "esp_spiffs.h"
+#include "esp_log.h"
 
 void app_main(void)
 {
-
+  // register js file system
+  // esp_vfs_spiffs_conf_t fs_conf = {
+  //   .
+  // }
+  // esp_vfs_spiffs_register();
 }
